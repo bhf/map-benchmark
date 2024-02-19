f99309b11222279839a7d30636e1855c3dff9752
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 12:24:12 +0000

    MB-20 Flatten out JSON to csv

---
+++ java/com/bhf/jmhresults/JMHJSONProcessor.java
@@ -0,0 +1,82 @@
+package com.bhf.jmhresults;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JMHJSONProcessor {
+
+ 
+  public void processJSON(String jsonFile, boolean printHeaders) {
+    JSONParser p = new JSONParser();
+    try {
+      Object parsed = p.parse(new FileReader(jsonFile));
+      JSONArray testDets = (JSONArray) parsed;
+
+      String singleHeaders = null;
+      List<StringBuilder> allLines = new ArrayList<StringBuilder>();
+
+      for (int i = 0; i < testDets.size(); i++) {
+        JSONObject el = (JSONObject) testDets.get(i);
+        JSONObject primaryMetric = (JSONObject) el.get("primaryMetric");
+        String primaryScoreVal = primaryMetric.get("score").toString();
+        JSONObject secondaryMetrics = (JSONObject) el.get("secondaryMetrics");
+
+        StringBuilder secondaryBuilder = new StringBuilder();
+        StringBuilder secondaryHeaders = new StringBuilder();
+        for (Object o : secondaryMetrics.keySet()) {
+
+          JSONObject sm = (JSONObject) secondaryMetrics.get(o);
+          String scoreVal = sm.get("score").toString();
+          secondaryBuilder.append(scoreVal + ",");
+          secondaryHeaders.append(o + ",");
+        }
+
+        StringBuilder paramsBuilder = new StringBuilder();
+        StringBuilder paramsHeaders = new StringBuilder();
+        JSONObject params = (JSONObject) el.get("params");
+
+        if(null!=params)
+        {
+          for (Object o : params.keySet()) {
+
+            String sm = params.get(o).toString();
+            paramsBuilder.append(sm + ",");
+            paramsHeaders.append(o + ",");
+          }
+        }
+
+        String benchmark = el.get("benchmark").toString();
+        StringBuilder rem = new StringBuilder();
+        StringBuilder headers = new StringBuilder();
+        headers.append("benchmark" + ",primaryScore,");
+        rem.append(benchmark + "," + primaryScoreVal + ",");
+
+        headers.append(paramsHeaders).append(secondaryHeaders);
+        rem.append(paramsBuilder).append(secondaryBuilder);
+        allLines.add(rem);
+
+        if (null == singleHeaders) {
+          singleHeaders = headers.toString();
+        }
+      }
+
+      if (printHeaders) {
+        System.out.println(singleHeaders);
+      }
+      for (StringBuilder sb : allLines) {
+        System.out.println(sb);
+      }
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
