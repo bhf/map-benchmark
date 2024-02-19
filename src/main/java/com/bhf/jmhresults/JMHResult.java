6d0c80373db0ba0187457836ca26fbe554ecaa4f
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 16:42:56 +0000

    MB-25 Modifying for direct fields for heuristic calculation

--- src/main/java/com/bhf/jmhresults/JMHResult.java
+++ src/main/java/com/bhf/jmhresults/JMHResult.java
@@ -5,15 +5,30 @@ import org.json.simple.JSONObject;
 
 public class JMHResult {
 
-    public final String benchmarkName;
-    public final BigDecimal primaryScore;
-    public final JSONObject secondaryScores;
-    public final JSONObject params;
+	public final String benchmarkName;
+	public final BigDecimal primaryScore;
+	public final JSONObject secondaryScores;
+	public final JSONObject params;
+	public double L1_dcache_loads;
+	public double LLC_loads;
+	public double LLC_load_misses;
+	public double L2_RQSTS_ALL_DEMAND_DATA_RD;
+	public double STALLS_TOTAL;
+	public double STALLS_L1D_MISS;
+	public double STALLS_L2_MISS;
+	public double STALLS_L3_MISS;
+	public double CYCLES_L1D_MISS;
+	public double CYCLES_L2_MISS;
+	public double CYCLES_L3_MISS;
+	public double STALLS_L1D_PENDING;
+	public double STALLS_L2_PENDING;
+	public double STALLS_LDM_PENDING;
 
-    public JMHResult(String benchmarkName, BigDecimal primaryScore, JSONObject secondaryScores, JSONObject params) {
-        this.benchmarkName = benchmarkName;
-        this.primaryScore = primaryScore;
-        this.secondaryScores = secondaryScores;
-        this.params = params;
-    }
+	public JMHResult(String benchmarkName, BigDecimal primaryScore, JSONObject secondaryScores, JSONObject params) {
+		this.benchmarkName = benchmarkName;
+		this.primaryScore = primaryScore;
+		this.secondaryScores = secondaryScores;
+		this.params = params;
+
+	}
 }
