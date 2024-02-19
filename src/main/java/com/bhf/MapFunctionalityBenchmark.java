aa98af07624f1ba1aacccb03d6f9204acf2131fe
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 11:22:27 +0000

    MB-11 Initial commit of interface

---
+++ src/main/java/com/bhf/MapFunctionalityBenchmark.java
@@ -0,0 +1,13 @@
+package com.bhf;
+
+/**
+ * Basic functionality to be implemented by the different
+ * classes testing the different implementations of maps
+ */
+public interface MapFunctionalityBenchmark {
+
+	public void benchPut();
+	public void benchGet();
+	public void benchRemove();
+	
+}
