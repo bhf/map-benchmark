328c173ee5137bf9054110e2f46935f4a172dd87
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 12:25:14 +0000

    MB-20 Rename to csv output

--- java/com/bhf/jmhresults/JMHJSONToCSVProcessor.java
+++ java/com/bhf/jmhresults/JMHJSONToCSVProcessor.java
@@ -9,7 +9,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class JMHJSONProcessor {
+public class JMHJSONToCSVProcessor {
 
  
   public void processJSON(String jsonFile, boolean printHeaders) {
