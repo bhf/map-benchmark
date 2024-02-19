55518cde610d7d0df5b480c262a021c9234fcb21
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 21:00:07 +0000

    MB-32 Adding tests

--- src/test/java/com/bhf/heuristics/L1SummaryHeuristicTest.java
+++ src/test/java/com/bhf/heuristics/L1SummaryHeuristicTest.java
@@ -2,24 +2,47 @@ package com.bhf.heuristics;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.bhf.jmhresults.JMHResult;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import com.bhf.jmhresults.JMHResult;
-
 class L1SummaryHeuristicTest {
 
-	@Test
-	void test() {
-		L1SummaryHeuristic heuristicCalc=new L1SummaryHeuristic();
-		String benchmarkName="TestHeuristic";
-		double primaryScore=123.4;
-		JSONObject secondaryScore=null;
-		JSONObject params=null;
-		JMHResult jmhRes=new JMHResult(benchmarkName, primaryScore, secondaryScore, params);
-		heuristicCalc.calculateHeuristic(jmhRes);
-		double badVal=heuristicCalc.getHeuristicValue();
-		assertTrue(0==badVal);
-	}
+    private static final int HEURISTIC_DEFAULT_VALUE = 0;
 
+    @Test
+    void testMissingValues() {
+        L1SummaryHeuristic heuristicCalc = new L1SummaryHeuristic();
+        String benchmarkName = "TestHeuristic";
+        double primaryScore = 123.4;
+        JSONObject secondaryScore = null;
+        JSONObject params = null;
+        JMHResult jmhRes = new JMHResult(benchmarkName, primaryScore, secondaryScore, params);
+        heuristicCalc.calculateHeuristic(jmhRes);
+        double badVal = heuristicCalc.getHeuristicValue();
+        assertTrue(HEURISTIC_DEFAULT_VALUE == badVal);
+    }
+
+    @Test
+    void testKnownValues() {
+        L1SummaryHeuristic heuristicCalc = new L1SummaryHeuristic();
+        String benchmarkName = "TestHeuristic";
+        double primaryScore = 123.4;
+        JSONObject secondaryScore = null;
+        JSONObject params = null;
+        JMHResult jmhRes = new JMHResult(benchmarkName, primaryScore, secondaryScore, params);
+        jmhRes.STALLS_L1D_MISS = 1;
+        jmhRes.L1_dcache_loads = 1;
+        heuristicCalc.calculateHeuristic(jmhRes);
+        double goodVal = heuristicCalc.getHeuristicValue();
+        assertTrue(1 == goodVal);
+    }
+
+    @Test
+    void testNull() {
+        L1SummaryHeuristic heuristicCalc = new L1SummaryHeuristic();
+        heuristicCalc.calculateHeuristic(null);
+        double goodVal = heuristicCalc.getHeuristicValue();
+        assertTrue(HEURISTIC_DEFAULT_VALUE == goodVal);
+    }
 }
