0a1a459176d061d0591a454e743862a430e56894
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-19 11:28:31 +0000

    MB-12 Initial commit of testing interface, added JUnit 5 dep to pom

---
+++ src/test/java/MapFunctionalityTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test basic map functionality
+ */
+public interface MapFunctionalityTest {
+
+	@Test
+	public void testPut();
+	
+	@Test
+	public void testGet();
+	
+	@Test
+	public void testRemove();
+	
+}
