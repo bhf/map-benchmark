732d83ccbdc0bce65533f59a096364d65ad3bff5
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-20 10:49:39 +0000

    MB-29 Structure for ratio based heuristics

---
+++ src/main/java/com/bhf/heuristics/RatioHeuristic.java
@@ -0,0 +1,36 @@
+package com.bhf.heuristics;
+
+import com.bhf.jmhresults.JMHResult;
+
+public class RatioHeuristic implements DerivedHeuristicCalculator {
+
+    final DerivedHeuristicCalculator numerator;
+    final DerivedHeuristicCalculator denominator;
+    double heuristicVal;
+
+    public RatioHeuristic(DerivedHeuristicCalculator numerator, DerivedHeuristicCalculator denominator) {
+        super();
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public void calculateHeuristic(JMHResult res) {
+        if (null != res) {
+            numerator.calculateHeuristic(res);
+            denominator.calculateHeuristic(res);
+
+            double numeratorVal = numerator.getHeuristicValue();
+            double denominatorVal = denominator.getHeuristicValue();
+
+            if (numeratorVal > 0 && denominatorVal > 0) {
+                heuristicVal = numeratorVal / denominatorVal;
+            }
+        }
+    }
+
+    @Override
+    public double getHeuristicValue() {
+        return heuristicVal;
+    }
+}
