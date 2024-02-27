2c8d9fa24cf12d152c70756c0040e0bdf9b6414f
Sanjeev Sarda <sanjeev.sarda@gmail.com>
2024-02-27 17:08:24 +0000

    MB-50 Interface expansion to allow basic unit testing of different map impls

--- src/main/java/com/bhf/map/MapGetBenchmark.java
+++ src/main/java/com/bhf/map/MapGetBenchmark.java
@@ -1,9 +1,26 @@
 package com.bhf.map;
 
+import java.util.Map;
+
 /**
  * Benchmark get operations
  */
-public interface MapGetBenchmark {
+public interface MapGetBenchmark<K,V> {
 
+    /**
+     * Get the expected Map key and values
+     * for unit testing map implementation
+     * @return
+     */
+    Map<K,V> getExpectedMap();
+    
+    /**
+     * Initialise the map being benchmarked with the 
+     * keys and values from the inputData - must be
+     * annotated as @Setup(Level.*)
+     * @param inputData
+     */
+    void initMap(Map<K,V> inputData);
+    
     void benchGet();
 }
--- src/main/java/com/bhf/map/MapPutBenchmark.java
+++ src/main/java/com/bhf/map/MapPutBenchmark.java
@@ -1,9 +1,30 @@
 package com.bhf.map;
 
+import java.util.Map;
+
 /**
  * Benchmark put operations
  */
-public interface MapPutBenchmark {
+public interface MapPutBenchmark<K,V> {
 
+    /**
+     * Get the expected Map after a put iteration
+     * for unit testing map implementations
+     * @return
+     */
+    Map<K,V> getExpectedMap();
+    
+    /**
+     * Initialise the map being benchmarked with the 
+     * keys and values from the inputData - must be
+     * annotated as @Setup(Level.*)
+     * @param inputData
+     */
+    void initMap(Map<K,V> inputData);
+    
+    /**
+     * Put operation on the map implementation being
+     * benchmarked - must be annotated as @Benchmark
+     */
     void benchPut();
 }
